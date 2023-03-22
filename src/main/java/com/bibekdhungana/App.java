288d4bf81f902400101b79dc1f9501e4582fa7e5
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-03-21 19:59:31 -0500

    Maven quickstart

--- src/main/java/com/bibekdhungana/App.java
+++ src/main/java/com/bibekdhungana/App.java
@@ -1,13 +1,16 @@
 package com.bibekdhungana;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("**************Started**********************");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("laptop.xml");
+        Laptop laptop = applicationContext.getBean("laptop", Laptop.class);
+        System.out.println(laptop);
+        ((AbstractApplicationContext) applicationContext).close();
+        System.out.println("**************End**********************");
     }
 }
