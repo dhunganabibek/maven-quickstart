8eb089fe08e3df57ce15a493f0383d08a5e6b02c
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-03-23 18:13:11 -0500

    Autowire code added

--- src/main/java/com/bibekdhungana/App.java
+++ src/main/java/com/bibekdhungana/App.java
@@ -6,11 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("**************Started**********************");
+        System.out.println("--------------------Started-------------------");
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("laptop.xml");
         Laptop laptop = applicationContext.getBean("laptop", Laptop.class);
         System.out.println(laptop);
         ((AbstractApplicationContext) applicationContext).close();
-        System.out.println("**************End**********************");
+        System.out.println("--------------------End------------------------");
     }
 }
